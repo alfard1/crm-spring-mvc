d1d7ced9f4cd05e7f184d264d771ccd11d074979
Pawel Wysokinski <pawel.wysokinski@gmail.com>
2019-09-03 23:15:07 +0200

    commit after revert

--- src/main/java/crm/config/CustomAuthenticationSuccessHandler.java
+++ src/main/java/crm/config/CustomAuthenticationSuccessHandler.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -17,15 +18,13 @@ import crm.service.UserService;
 @Component
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
+    @Autowired
     private UserService userService;
-
-	public CustomAuthenticationSuccessHandler(UserService userService) {
-		this.userService = userService;
-	}
-
+	
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
-			throws IOException {
+			throws IOException, ServletException {
+
 
 
 		String userName = authentication.getName();
